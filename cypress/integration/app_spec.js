3884d94a8eeeb060b6da53b99bfe045ce0af3a17
younahshin-dev <roaving@gmail.com>
2022-03-01 17:45:08 +0900

    using @testing-library/cypress/add-commands

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,37 +1,79 @@
+/// <reference types="cypress" />
+import '@testing-library/cypress/add-commands';
+
 describe('My First Test', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/');
   })
-  it('Clicking Add button shoud add Habit to list', () => {
-    cy.get('input').should('have.attr', 'placeholder', 'Habit').type('Eating')
-    cy.contains('Add').click()
+
+  // it('Clicking Add button shoud add Habit to list', () => {
+  //   cy.get('input').should('have.attr', 'placeholder', 'Habit').type('Eating')
+  //   cy.contains('Add').click()
+  // })
+
+  // it('delete Habit', () => {
+  //   cy.get('.habit-delete').first().as('firstBtn')
+  //   cy.get('@firstBtn').click()
+  // })
+
+  // it('increase Habit', () => {
+  //   cy.get('.habit-increase').each(($el, index, $list) => {
+  //     console.log($el, index, $list)
+  //     $el.click()
+  //   })
+  // })
+
+  // it('decrease Habit', () => {
+  //   cy.get('.habit-increase').each(($el, index, $list) => {
+  //     console.log($el, index, $list)
+  //     $el.click()
+  //   })
+  //   cy.get('.habit-decrease').first().click()
+  // })
+
+  // it('reset All Habits', () => {
+  //   cy.get('.habit-increase').each(($el, index, $list) => {
+  //     console.log($el, index, $list)
+  //     $el.click()
+  //   })
+  //   cy.get('.habits-reset').click()
+  // })
+
+   // => Testing Library 사용으로 변경
+   it('render', () => {
+     cy.findByText('Habit Tracker').should('exist');
+   })
+
+   it('Clicking Add button shoud add Habit to list', () => {
+    cy.findByPlaceholderText('Habit').type('Eating')
+    cy.findByText('Add').click()
   })
 
   it('delete Habit', () => {
-    cy.get('.habit-delete').first().as('firstBtn')
+    cy.findAllByTitle('deleteBtn').first().as('firstBtn')
     cy.get('@firstBtn').click()
   })
 
   it('increase Habit', () => {
-    cy.get('.habit-increase').each(($el, index, $list) => {
+    cy.findAllByTitle('increseBtn').each(($el, index, $list) => {
       console.log($el, index, $list)
       $el.click()
     })
   })
 
   it('decrease Habit', () => {
-    cy.get('.habit-increase').each(($el, index, $list) => {
+    cy.findAllByTitle('increseBtn').each(($el, index, $list) => {
       console.log($el, index, $list)
       $el.click()
     })
-    cy.get('.habit-decrease').first().click()
+    cy.findAllByTitle('decreseBtn').first().click()
   })
 
   it('reset All Habits', () => {
-    cy.get('.habit-increase').each(($el, index, $list) => {
+    cy.findAllByTitle('increseBtn').each(($el, index, $list) => {
       console.log($el, index, $list)
       $el.click()
     })
-    cy.get('.habits-reset').click()
+    cy.findByText('Reset All').click()
   })
 })
